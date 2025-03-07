77ac89866e54e9c758549a07b8755f91ce5cfb19
FezilShock <slav4ek228666@mail.ru>
2025-03-07 21:50:40 +0300

    change set.c

--- set.c
+++ set.c
@@ -4,17 +4,17 @@
 #include <string.h>
 
 typedef struct n{
-    int val;
+    double val;
     struct n* next;
 }node;
 
 typedef struct{
-    node* set_pt;
+    node* first_node;
     node* now;
     int size;
 }set;
 
-node* create_node(int val){
+node* create_node(double val){
     node* new_node = (node *)malloc(sizeof(node));
     new_node->val = val;
     new_node->next = NULL;
@@ -23,14 +23,14 @@ node* create_node(int val){
 
 set* createSet(void){
     set* new_set = (set*)malloc(sizeof(set));
-    new_set->set_pt = NULL;
+    new_set->first_node = NULL;
     new_set->size = 0;
     return new_set;
 }
 
-bool setSearch(set* set, int var){
+bool setSearch(set* set, double var){
     if(set->size){
-        node* curNode = set->set_pt;
+        node* curNode = set->first_node;
         while(curNode != NULL){
             if(curNode->val == var) return curNode;
             curNode = curNode->next;
@@ -40,39 +40,64 @@ bool setSearch(set* set, int var){
 }
 
 void setPush(set* set){
-    int new_var;
+    double new_var;
+    char ch;
     char res = 0;
-    scanf("%d", &new_var);
+    scanf("%lf", &new_var);
     node* tmp = create_node(new_var);
-    set->set_pt = tmp;
+    set->first_node = tmp;
     set->now = tmp;
     ++set->size;
     do{
-        char res = scanf("%d", &new_var);
+        res = scanf("%lf", &new_var);
         if (!setSearch(set, new_var)){
             node* tmp = create_node(new_var);
             set->now->next = tmp;
             set->now = tmp;
             ++set->size;
         }
-    }while(new_var != 0);
-}
-
-void setPush2(node* last, set* set){
-
+        if (res == 0 && (ch = getchar()) == 'q') break;
+    }while(true);
 }
 
 void setOutput(set* set){
-    node* curNode = set->set_pt;
+    node* curNode = set->first_node;
     while(curNode != NULL){
-        printf("%d ", curNode->val);
+        printf("%.4f ", curNode->val);
         curNode = curNode->next;
+    }
+}
+
+int convert(int dv){
+    int res = 0;
+    long j = 1;
+    while(dv > 0){
+        res += (dv % 2) * j;
+        dv /= 2;
+        j *= 10;
+    }
+    return res;
+}
+
+void binconvert(set* main_set, set* bin_set){
+    node* curNode = main_set->first_node;  
+    node* tmp = create_node(convert(curNode->val));
+    bin_set->first_node = tmp;
+    bin_set->now = tmp;
+    ++bin_set->size;
+    curNode = curNode->next;
+    while(curNode != NULL){
+        node* tmp = create_node(convert(curNode->val));
+        bin_set->now->next = tmp;
+        bin_set->now = tmp;
+        curNode = curNode->next;
+        ++bin_set->size;
     }
 }
 
 void freeset(set* set){
     if(set->size){
-        node* curNode = set->set_pt;
+        node* curNode = set->first_node;
         while(curNode != NULL){
             node* delNode = curNode;
             curNode = curNode->next;
@@ -83,11 +108,25 @@ void freeset(set* set){
     free(set);
 }
 
+double modulo(double a){
+    if(a >= 0){
+        int b = (int) a;
+        return (a-b);
+    }else{
+        int b = (int) a;
+        return (b-a);
+    }
+}
+
 int main(void)
 {
     set* main_set = createSet();
+    set* bin_set = createSet();
     setPush(main_set);
+    binconvert(main_set, bin_set);
     setOutput(main_set);
+    putchar('\n');
+    setOutput(bin_set);
     freeset(main_set);
     return 0;
 }
